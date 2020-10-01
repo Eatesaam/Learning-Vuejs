56c91294ff1fbc4b400f02b077d71a083ff80933
Muhammad Eatesaam Ahmad <eatesaam@gmail.com>
2020-10-01 20:55:35 +0500

    learning vuejs

--- binding-classes-with-object-and-array/main.js
+++ binding-classes-with-object-and-array/main.js
@@ -4,8 +4,8 @@ var app = new Vue({
     data: {
         inputwidth: 60,
         sliderStatus: true,
-        labelArr: ['font-weight-bold', 'mt-5'],
-        maximum: 70,
+        labelArr: ['font-weight-bold', 'mr-2'],
+        maximum: 99,
         products: null,
         cart: []
     },
@@ -15,6 +15,21 @@ var app = new Vue({
         }
     },
     methods: {
+        beforeEnter: function(el){
+            el.className = 'd-none'
+        },
+        enter: function(el){
+            var delay = el.dataset.index * 100;
+            setTimeout(function(){
+                el.className = 'row d-flex mb-3 align-items-center animated fadeInRight';
+            }, delay);
+        },
+        leave: function(el){
+            var delay = el.dataset.index * 100;
+            setTimeout(function(){
+                el.className = 'row d-flex mb-3 align-items-center animated fadeOutRight';
+            }, delay);
+        },
         addItem: function(product){
             this.cart.push(product);
         }
